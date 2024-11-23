da42fcef1cab38c4815298f85f7e76453c991250
Luqmanoffical <lk3281751@gmail.com>
2024-11-23 16:44:34 +0500

    ok

--- src/components/FeaturedListings.jsx
+++ src/components/FeaturedListings.jsx
@@ -12,18 +12,18 @@ const FeaturedListings = () => {
       <div className="container mx-auto text-center">
         <h2 className="text-3xl font-semibold mb-6">Hot Properties for Sale</h2>
 
-        <div className='flex w-full gap-4'> {/* Added gap between flex items */}
+        <div className='flex w-full '> {/* Added gap between flex items */}
 
           {/* Left Half of the Section */}
           <div className='w-1/2'>
-            <div className="p-4 bg-white mb-4 relative group">
-              <img src={P1} alt="P1" className="w-full h-[17rem] object-cover object-top  rounded-md" />
+            <div className="p-4 bg-white  relative group">
+              <img src={P1} alt="P1" className="w-full h-[18rem] object-cover object-top  rounded-md" />
               {/* Button that appears when hovering over the image */}
               <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2      bg-[#ffffff6b] border w-[9rem] border-white text-white font-semibold rounded-2xl shadow-md opacity-0 group-hover:opacity-100 transition duration-300   py-3">
                 View Property
               </button>
             </div>
-            <div className='flex w-full gap-4'> {/* Added gap between image cards */}
+            <div className='flex w-full '> {/* Added gap between image cards */}
               <div className="w-1/2 p-4 bg-white relative group">
                 <img src={P2} alt="P2" className="w-full h-auto object-cover rounded-md" />
                 <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2      bg-[#ffffff6b] border w-[9rem] border-white text-white font-semibold rounded-2xl shadow-md opacity-0 group-hover:opacity-100 transition duration-300   py-3">
@@ -41,7 +41,7 @@ const FeaturedListings = () => {
 
           {/* Right Half of the Section */}
           <div className='w-1/2'>
-            <div className='flex w-full gap-4'> {/* Added gap between image cards */}
+            <div className='flex w-full '> {/* Added gap between image cards */}
               <div className="w-1/2 p-4 bg-white relative group">
                 <img src={P4} alt="P2" className="w-full h-auto object-cover rounded-md" />
                 <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2      bg-[#ffffff6b] border w-[9rem] border-white text-white font-semibold rounded-2xl shadow-md opacity-0 group-hover:opacity-100 transition duration-300   py-3">
@@ -55,8 +55,8 @@ const FeaturedListings = () => {
                 </button>
               </div>
             </div>
-            <div className="p-4 bg-white mt-4 relative group">
-              <img src={P5} alt="P1" className="w-full h-[17rem] object-cover  rounded-md" />
+            <div className="p-4 bg-white relative group">
+              <img src={P5} alt="P1" className="w-full h-[18rem] object-cover  rounded-md" />
               <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2    bg-[#ffffff6b] border w-[9rem] border-white text-white font-semibold rounded-2xl shadow-md opacity-0 group-hover:opacity-100 transition duration-300   py-3">
                 View Property
               </button>
